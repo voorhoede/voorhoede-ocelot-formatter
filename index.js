3d606cba08e6cac54eacd024ea8a0f7d87099f46
Sanne Veroude <sanne@voorhoede.nl>
2016-09-14 09:58:15 +0200

    feat(templates): add toc

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const markdownToHtml = require('./lib/markdown-to-html');
 const getPageData = require('./lib/get-page-data');
 const renderHtml = require('./lib/render-html');
+const getHeadings = require('./lib/get-headings');
 
 function formatter(filename, dest) {
 
-    markdownToHtml(filename)
-        .then((html) => getPageData(html))
-        .then((data) => renderHtml(data, dest))
+    return markdownToHtml(filename)
+        .then(html => getPageData(html))
+        .then(data => addHeadings(data))
+        .then(data => renderHtml(data, dest))
+}
+
+function addHeadings(data) {
+    return Object.assign({}, data, { headings: getHeadings(data.body) });
 }
 
 module.exports = formatter;
