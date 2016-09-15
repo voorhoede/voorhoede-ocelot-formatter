407aca31b5fa9b83eae36b50feda611ad0a55625
Sanne Veroude <sanne@voorhoede.nl>
2016-09-15 12:10:16 +0200

    chore(test): add tape for testing

---
+++ test/get-page-data.test.js
@@ -0,0 +1,34 @@
+const test = require('tape');
+const getPageData = require('../lib/get-page-data');
+
+test('should get the title', function(t) {
+    const html = '<h1>Hello World</h1>';
+    const output = getPageData(html);
+
+    t.equal(output.title, 'Hello World');
+    t.end();
+});
+
+test('should get the first h1 as title', function(t) {
+    const html = '<h1>First title</h1><h1>Second title</h1>';
+    const output = getPageData(html);
+
+    t.equal(output.title, 'First title');
+    t.end();
+});
+
+test('should get the first p as subtitle', function(t) {
+    const html = '<h1>Hello World</h1><p>Text</p><p>Some more text</p>';
+    const output = getPageData(html);
+
+    t.equal(output.subtitle, 'Text');
+    t.end();
+});
+
+test('should get the body', function(t) {
+    const html = '<h1>Hello World</h1><p>Text</p><p>Some more text</p>';
+    const output = getPageData(html);
+
+    t.equal(output.body, '<p>Some more text</p>');
+    t.end();
+});
