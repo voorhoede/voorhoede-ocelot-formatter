d833bfd08377e580b993c8dd71cf75dbc4af8925
Sanne Veroude <sanne@voorhoede.nl>
2016-10-03 12:51:08 +0200

    refactor(test): simplify test
    by letting getPageProperties return a Promise

--- test/main.js
+++ test/main.js
@@ -2,31 +2,24 @@ const test = require('tape');
 const cheerio = require('cheerio');
 const formatter = require('../index');
 const markdown = getMarkdown();
-const html = formatter(markdown);
 
 test('should render the title as document title', t => {
-    html.then(html => {
-        const page = getPageProperties(html);
-
-        t.equal(page.documentTitle, 'Hello World');
+    getPageProperties(markdown).then(page => {
+        t.equal(page.title, 'Hello World');
         t.end();
     });
 });
 
-test('should render the title as h1', t => {
-    html.then(html => {
-        const page = getPageProperties(html);
-
-        t.equal(page.pageTitle, 'Hello World');
+test('should render the title as page header title', t => {
+    getPageProperties(markdown).then(page => {
+        t.equal(page.pageHeaderTitle, 'Hello World');
         t.end();
     });
 });
 
 test('should render a navigation item for each heading', t => {
-    html.then(html => {
-        const page = getPageProperties(html);
-
-        t.equal(page.headings.length, 3);
+    getPageProperties(markdown).then(page => {
+        t.equal(page.navigationItems.length, 3);
         t.end();
     });
 });
@@ -41,14 +34,14 @@ some more text
 even more text`
 }
 
-function getPageProperties(html) {
-    const $ = cheerio.load(html);
-    const $html = $('html');
-
-    return {
-        documentTitle: $html.find('title').html(),
-        pageTitle: $html.find('.page-header-title').html(),
-        headings: $html.find('*[class^="toc-level"]'),
-        body: $html.find('.page-content section').html()
-    }
+function getPageProperties(markdown) {
+    return formatter(markdown).then(html => {
+        const $ = cheerio.load(html);
+        const $html = $('html');
+        return {
+            title: $html.find('title').html(),
+            pageHeaderTitle: $html.find('.page-header-title').html(),
+            navigationItems: $html.find('*[class^="toc-level"]')
+        };
+    });
 }
