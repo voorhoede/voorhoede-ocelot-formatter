6d586061cbbf05e558792489d6f180b9de15960b
Sanne Veroude <sanne@voorhoede.nl>
2016-10-03 14:49:00 +0200

    refactor(test): use cheerio to check the snippet

--- test/render-code.js
+++ test/render-code.js
@@ -1,18 +1,25 @@
 const test = require('tape');
 const renderCode = require('../lib/render-code');
+const cheerio = require('cheerio');
 const code = getCode();
 
 test('should render code with javascript language', t => {
     const snippet = renderCode(code, 'javascript');
+    const $ = cheerio.load(snippet);
 
-    t.equal(snippet, '<pre class="language-javascript"><code><span class="token keyword" >const</span> foo <span class="token operator" >=</span> bar<span class="token punctuation" >;</span></code></pre>');
+    t.equal($('pre').attr('class'), 'language-javascript');
+    t.notEqual($('span').length, 0);
     t.end();
 });
 
 test('should render code with unknown language', t => {
     const snippet = renderCode(code);
+    const $ = cheerio.load(snippet);
 
-    t.equal(snippet, '<pre class="language-unknown"><code>const foo = bar;</code></pre>');
+    console.log(snippet);
+
+    t.equal($('pre').attr('class'), 'language-unknown');
+    t.equal($('span').length, 0);
     t.end();
 });
 
