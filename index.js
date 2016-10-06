913955785425b0cb6068ba31f8e60d550ad1adeb
Sanne Veroude <sanne@voorhoede.nl>
2016-10-06 15:34:37 +0200

    feat(main): add language option

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const getPageData = require('./lib/get-page-data');
 const renderHtml = require('./lib/render-html');
 const getHeadings = require('./lib/get-headings');
 
-function formatter(markdown) {
+function formatter(markdown, options) {
+    options = Object.assign({ language: 'en' }, options);
 
     return markdownToHtml(markdown)
         .then(html => getPageData(html))
-        .then(data => addHeadings(data))
+        .then(data => addData(data, options))
         .then(data => renderHtml(data))
 }
 
-function addHeadings(data) {
-    return Object.assign({}, data, { headings: getHeadings(data.body) });
+function addData(data, options) {
+    return Object.assign({}, data, {
+        headings: getHeadings(data.body),
+        options: options
+    });
 }
 
 module.exports = formatter;
