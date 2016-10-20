f19048d918e2e96bbb10168f4d637ab2c63299df
Sanne Veroude <sanne@voorhoede.nl>
2016-10-20 16:25:00 +0200

    feat(bin): add option to not render table of contents

--- bin/index.js
+++ bin/index.js
@@ -16,6 +16,7 @@ const program = require('commander')
     .usage('<file> [options]')
     .option('-l, --lang [value]', 'specify language of page content')
     .option('-t, --toc [range]', 'specify min and max level of table of contents', range)
+    .option('-n, --noToc', 'do not render table of contents')
     .parse(process.argv);
 
 const srcFilename = path.join(program.args[0]);
@@ -25,7 +26,7 @@ program.toc = program.toc || [];
 fs.readFileAsync(srcFilename, 'utf8')
     .then(readme => formatter(readme, {
         language: program.lang,
-        toc: { minLevel: program.toc[0], maxLevel: program.toc[1] }
+        toc: program.noToc ? false : { minLevel: program.toc[0], maxLevel: program.toc[1] }
     }))
     .then(html => console.log(html))
     .catch(err => console.error(err));
