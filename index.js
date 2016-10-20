15c924c745922f9ff5fc8f495f1875f4a1e176db
Sanne Veroude <sanne@voorhoede.nl>
2016-10-20 15:10:02 +0200

    refactor(config-data): move to separate file

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const markdownToHtml = require('./lib/markdown-to-html');
 const getPageData = require('./lib/get-page-data');
+const configData = require('./lib/config-data');
 const renderHtml = require('./lib/render-html');
-const getHeadings = require('./lib/get-headings');
 
 function formatter(markdown, options) {
-    options = Object.assign({
-        language: 'en',
-        toc: { minLevel: 2, maxLevel: 6 }
-    }, options);
 
     return markdownToHtml(markdown)
         .then(html => getPageData(html))
-        .then(data => addData(data, options))
+        .then(data => configData(data, options))
         .then(data => renderHtml(data))
-}
-
-function addData(data, options) {
-    return Object.assign({}, data, {
-        headings: getHeadings(data.body),
-        options: options
-    });
 }
 
 module.exports = formatter;
