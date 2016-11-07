cf708bdf64ff7af73de471beef748d0d46f4e159
Sanne Veroude <veroude.sanne@gmail.com>
2016-11-07 11:08:30 +0100

    feat(bin): add --output option to allow to write to a file (#16)
    closes #4
    
    * docs(main): add missing `--output` flag; switch CLI options to have the simplest use case first
    * fix(cli): specify that --output options takes an argument
    * refactor(cli): rename and move to variable
    * chore: use mkdirp to create new directories

--- README.md
+++ README.md
@@ -18,10 +18,16 @@ You can use Voorhoede Ocelot Formatter both as a CLI tool and programmatically i
 
 ### CLI
 
-Convert markdown file to html and log output to the console:
+Convert markdown file to HTML and **log output to the console**:
 
 ```bash
 $ formatter README.md
+```
+
+Convert markdown file to HTML and **output content to an HTML file** using `--output`:
+
+```bash
+$ formatter README.md --output path/to/output/file.html
 ```
 
 ### JS
--- bin/index.js
+++ bin/index.js
@@ -3,19 +3,30 @@
 const formatter = require('../index');
 const pkg = require('../package');
 const path = require('path');
+const promisify = require('bluebird').promisify;
 const promisifyAll = require('bluebird').promisifyAll;
+const makeDir = promisify(require('mkdirp'));
 const fs = promisifyAll(require('fs'));
 
 const program = require('commander')
     .version(pkg.version)
     .description(`${pkg.name} (v${pkg.version}): ${pkg.description}`)
-    .usage('<file>')
-    .arguments('<file>')
+    .usage('<file> [options]')
+    .option('-o, --output <file>', 'write to HTML file')
     .parse(process.argv);
 
-const srcFilename = path.join(program.args[0]);
+const srcFile = path.join(program.args[0]);
+const destFile = program.output;
 
-fs.readFileAsync(srcFilename, 'utf8')
-    .then(readme => formatter(readme))
-    .then(html => console.log(html))
-    .catch(err => console.error(err));
+const format = fs.readFileAsync(srcFile, 'utf8')
+    .then(readme => formatter(readme));
+
+if (program.output) {
+    format.then(html => {
+        makeDir(path.dirname(destFile))
+            .then(() => fs.writeFileAsync(destFile, html))
+            .catch(err => console.error(err));
+    })
+} else {
+    format.then(html => console.log(html))
+}
