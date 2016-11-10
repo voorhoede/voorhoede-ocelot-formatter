bfd080fc6f44a80f0b524d01134bbf1350f8e15b
Sanne Veroude <sanne@voorhoede.nl>
2016-11-10 11:00:50 +0100

    feat(bin): add github options

--- bin/index.js
+++ bin/index.js
@@ -20,6 +20,7 @@ const program = require('commander')
     .option('-l, --lang [value]', 'specify language of page content')
     .option('-t, --toc [range]', 'specify min and max level of table of contents', range)
     .option('-n, --noToc', 'do not render table of contents')
+    .option('-g, --github [value]', 'add link to Github page')
     .parse(process.argv);
 
 const srcFile = path.join(program.args[0]);
@@ -30,7 +31,8 @@ program.toc = program.toc || [];
 const format = fs.readFileAsync(srcFile, 'utf8')
     .then(readme => formatter(readme, {
         language: program.lang,
-        toc: program.noToc ? false : { minLevel: program.toc[0], maxLevel: program.toc[1] }
+        toc: program.noToc ? false : { minLevel: program.toc[0], maxLevel: program.toc[1] },
+        github: program.github
     }));
 
 if (program.output) {
